ffb89178b58e28efac80fdadf971aa4c821e0f1f
Dabee Noh <movie2529@naver.com>
2022-02-01 01:34:18 +0900

    Initial commit

---
+++ src/main/java/com/dababy/springsecurityjwtsimple/SpringSecurityJwtSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.dababy.springsecurityjwtsimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtSimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityJwtSimpleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dababy/springsecurityjwtsimple/SpringSecurityJwtSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dababy.springsecurityjwtsimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtSimpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
