5b46cee33b8f60f1efaebda75615974471205f43
Dabee Noh <movie2529@naver.com>
2022-02-01 15:29:57 +0900

    Create a method that returns an Authentication object using the information contained in the Token

--- src/main/java/com/dababy/springsecurityjwtsimple/jwt/TokenProvider.java
+++ src/main/java/com/dababy/springsecurityjwtsimple/jwt/TokenProvider.java
@@ -1,17 +1,23 @@
 package com.dababy.springsecurityjwtsimple.jwt;
 
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
 import java.util.stream.Collectors;
 
@@ -53,6 +59,34 @@ public class TokenProvider implements InitializingBean {
                 .setExpiration(validity)
                 .compact();
     }
+
+    // Token에 담겨있는 정보를 이용해 Authentication 객체를 리턴하는 메소드 생성
+
+    /*
+    1. 토큰을 파라미터로 받아서, 토큰을 이용해서 클레임을 만든다.
+    2. 클레임에서 권한 정보들을 빼내어, 권한 정보를 이용 유저 객체를 만든다.
+    3. 유저 정보와, 토큰, 권한 정보를 이용해서 최종적으로 Authentication 객체를 리턴한다.
+     */
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = Jwts
+                .parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        Collection<? extends GrantedAuthority> authorities =
+                Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+
+        User principal = new User(claims.getSubject(), "", authorities);
+
+        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
+    }
+
+
 
 
 }
