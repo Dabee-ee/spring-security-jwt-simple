bc1f5cedfbfd76cc9fe48331be19f2f8e1ebcccc
Dabee Noh <movie2529@naver.com>
2022-02-01 15:18:18 +0900

    Assign secret value to key variable

---
+++ src/main/java/com/dababy/springsecurityjwtsimple/jwt/TokenProvider.java
@@ -0,0 +1,32 @@
+package com.dababy.springsecurityjwtsimple.jwt;
+
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+public class TokenProvider implements InitializingBean {
+
+    private final String secret;
+    private final long tokenValidityInMilliseconds;
+    private Key key;
+
+    // key 변수에 secret 값 할당
+    public TokenProvider(
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.token-validity-in-seconds}") long tokenValidityInSeconds) {
+        this.secret = secret;
+        this.tokenValidityInMilliseconds = tokenValidityInSeconds * 1000;
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+}
